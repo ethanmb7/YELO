dee96523b1fdfccfb49157dbe53360649d370466
ethanmb7 <ethan.mbn@gmail.com>
2024-10-02 00:26:37 +0200

    server.js

--- server.js
+++ server.js
@@ -11,37 +11,31 @@ let users = {}; // Objet pour suivre les utilisateurs connectés
 
 app.use(express.static('public'));
 
-// Gestion des connexions Socket.io
 io.on('connection', (socket) => {
     console.log('Nouvel utilisateur connecté : ' + socket.id);
 
-    // Ajouter l'utilisateur à la liste
     socket.on('setUsername', (username) => {
         users[socket.id] = username;
-        io.emit('userList', Object.values(users)); // Mettre à jour la liste des utilisateurs
-        socket.emit('messageHistory', messages); // Envoyer l'historique des messages
+        io.emit('userList', Object.values(users));
+        socket.emit('messageHistory', messages);
     });
 
-    // Écouter les messages envoyés par les utilisateurs
     socket.on('sendMessage', (data) => {
-        messages.push(data); // Stocker le message dans l'historique
-        io.emit('receiveMessage', data); // Émettre le message à tous les utilisateurs
+        messages.push(data);
+        io.emit('receiveMessage', data);
     });
 
-    // Indiquer qu'un utilisateur est en train d'écrire
     socket.on('typing', () => {
         socket.broadcast.emit('userTyping', users[socket.id]);
     });
 
-    // Gérer la déconnexion
     socket.on('disconnect', () => {
         console.log('Utilisateur déconnecté : ' + socket.id);
-        delete users[socket.id]; // Supprimer l'utilisateur de la liste
-        io.emit('userList', Object.values(users)); // Mettre à jour la liste des utilisateurs
+        delete users[socket.id];
+        io.emit('userList', Object.values(users));
     });
 });
 
-// Démarrer le serveur
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
